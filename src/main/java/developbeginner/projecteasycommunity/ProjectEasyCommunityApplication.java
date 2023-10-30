0f48161eeda1a2a3f8a81587c79e988d9b23dadd
developbeginner <148424130+developbeginner@users.noreply.github.com>
2023-10-30 20:38:20 +0900

    init: 스프링 프로젝트 생성
    - Intellij Spring Initializr로 생성.
    - Spring Web MVC 라이브러리 추가.
    
    Related to: #1

---
+++ src/main/java/developbeginner/projecteasycommunity/ProjectEasyCommunityApplication.java
@@ -0,0 +1,13 @@
+package developbeginner.projecteasycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEasyCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEasyCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/developbeginner/projecteasycommunity/ProjectEasyCommunityApplicationTests.java
@@ -0,0 +1,11 @@
+package developbeginner.projecteasycommunity;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectEasyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
