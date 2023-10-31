b825b15efb3848a15b68af975364b4923f0be616
developbeginner <148424130+developbeginner@users.noreply.github.com>
2023-10-31 21:54:41 +0900

    chore: 데이터베이스 연동 설정
    - DB 연동에 필요한 라이브러리 추가
    - application.yml 프로필 분리 (로컬=dev, 배포=prod)
    - 테스트 환경 설정 파일 추가
    
    Related to: #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,17 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot Starter Web
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Spring Boot Starter Test 3.1.5
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.5")
+    // MariaDB Java Client 3.2.0
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.2.0")
+    // H2 Database Engine 2.2.224
+    testImplementation("com.h2database:h2:2.2.224")
+    // MyBatis Spring Boot Starter 3.0.2
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2")
 }
 
 tasks.withType<Test> {
