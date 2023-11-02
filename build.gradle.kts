78d28ca213f9e5bfc9a2ae20f7921a539f9f7c89
developbeginner <148424130+developbeginner@users.noreply.github.com>
2023-11-02 11:01:25 +0900

    chore: 초기 라이브러리 추가
    - Thymeleaf, Lombok, Devtools 라이브러리 추가
    - 라이브러리 관련 설정 추가
    - 워크플로 주석 수정
    
    Related to: #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,17 +16,22 @@ repositories {
 }
 
 dependencies {
-    // Spring Boot Starter Web
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Spring Boot Starter Web 3.1.5
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
     // Spring Boot Starter Test 3.1.5
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.5")
+    // Spring Boot Starter Thymeleaf 3.1.5
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.1.5")
+    // Spring Boot Devtools 3.1.5
+    implementation("org.springframework.boot:spring-boot-devtools:3.1.5")
     // MariaDB Java Client 3.2.0
     implementation("org.mariadb.jdbc:mariadb-java-client:3.2.0")
     // H2 Database Engine 2.2.224
     testImplementation("com.h2database:h2:2.2.224")
     // MyBatis Spring Boot Starter 3.0.2
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2")
+    // Lombok 1.18.30
+    compileOnly("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.withType<Test> {
